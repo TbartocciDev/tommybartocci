3eac31e7b57b67ae9b27cfe554478f2302319e1e
TbartocciDev <tommybartocci@gmail.com>
2023-07-23 21:09:50 -0400

    home page added

---
+++ src/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <main className="HomePage"
+            style={{
+                height:"100vh",
+                width:"100vw",
+                backgroundColor:'green'}}>
+            <h1>Home Page</h1>
+        </main>
+    )
+}
