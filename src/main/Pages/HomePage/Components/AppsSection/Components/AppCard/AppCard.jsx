3c008de0883e2bc226e2712bd4cbb27ea9df04ff
TbartocciDev <tommybartocci@gmail.com>
2023-07-28 11:37:30 -0400

    session storage update

--- src/main/Pages/HomePage/Components/AppsSection/Components/AppCard/AppCard.jsx
+++ src/main/Pages/HomePage/Components/AppsSection/Components/AppCard/AppCard.jsx
@@ -5,19 +5,19 @@ import { useState } from "react";
 
 export default function AppCard({ data  }) {
 
-    if (!sessionStorage.getItem(`splashMode`)) {
-        sessionStorage.setItem(`splashMode`,'light')
+    if (!sessionStorage.getItem(`splashMode${data.index}`)) {
+        sessionStorage.setItem(`splashMode${data.index}`,'light')
     }
     
-    const [mode, setMode] = useState(sessionStorage.getItem(`splashMode`));
+    const [mode, setMode] = useState(sessionStorage.getItem(`splashMode${data.index}`));
 
     function handleAppClick() {
         if (mode === "light") {
-            sessionStorage.setItem(`splashMode`,'dark')
-            setMode(sessionStorage.getItem(`splashMode`))
+            sessionStorage.setItem(`splashMode${data.index}`,'dark')
+            setMode(sessionStorage.getItem(`splashMode${data.index}`))
         } else {
-            sessionStorage.setItem(`splashMode`,'light')
-            setMode(sessionStorage.getItem(`splashMode`))
+            sessionStorage.setItem(`splashMode${data.index}`,'light')
+            setMode(sessionStorage.getItem(`splashMode${data.index}`))
         }
     }
     
